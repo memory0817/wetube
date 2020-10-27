6db31f02417ecb370467904aa5577ed99d9b93dc
memory <memory140720@gmail.com>
2020-10-27 23:00:40 +0900

    2020.10.27 23:00 작업물 커밋

--- index.js
+++ index.js
@@ -8,5 +8,18 @@ function handleListening() {
     console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+function handleHome(req,res) {
+   // console.log(req);
+    res.send('hello from home');
+}
+
+function handleProfile(req,res) {
+    res.send("you are on my profile");
+}
+
+
+app.get("/",handleHome);
+
+app.get("/profile",handleProfile);
 
 app.listen(PORT, handleListening);
