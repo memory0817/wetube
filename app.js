34fdc6ee3564f51f758888b9b63fdfced19a56e6
memory <memory140720@gmail.com>
2020-12-13 00:29:21 +0900

    2020.12.12 변경사항 커밋

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter} from "./router";
+import userRouter from "./routers/uesrRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
 
-
-const handleHome = (req,res) =>  res.send('hello from home22');
-
-const handleProfile = (req,res) =>  res.send("you are on my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -18,11 +16,9 @@ app.use(bodyParser.urlencoded({ extended:true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-
-app.get("/profile",handleProfile);
-
+app.use("/", globalRouter);
 app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 export default app;
 
